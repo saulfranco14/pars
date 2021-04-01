4d9fbc14e7d511455efee529b9495d3cc14a738e
Saul Franco <saul.franco@tucanton.com>
2021-04-01 11:36:29 -0600

    Se agrega styled-component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,21 @@
 import * as React from "react"
+import styled from "styled-components"
+
+const TitleHeader = styled.h1`
+  font-size: 28px;
+  background-color: red;
+  color: white;
+
+`
 
 const Header = () => {
   return ( 
-   <React.Fragment>
-     <h1>Header</h1>
-   </React.Fragment>
+    <React.Fragment>
+      <TitleHeader>Header</TitleHeader>
+    </React.Fragment>
    );
 }
  
 export default Header;
+
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
 import Header from './header';
+import Helmet from 'react-helmet';
 
 const Layout = ( props ) => {
   return ( 
     <Fragment>
+      <Helmet>
+        <title> Pars </title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@700&display=swap" rel="stylesheet" />
+      </Helmet>
       <Header />
       {props.children}
     </Fragment>
